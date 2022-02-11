012eead41d24db3f63e5ff7ba956454ce5963cdf
Debo <55208382+adetijani@users.noreply.github.com>
2022-02-11 01:06:07 +0100

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,31 @@
+//Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+console.log(typeof x, adebowale, boolean, emptyvalue, firstname)
+
+//Declaring variable to name and others
+let Name= adebowale
+let lastname= Tijani
+let country= Nigeria
+let file= jslogger
+console.log(firstname, lastname, country, file)
+
+
+
+//declaring variable without value//
+let A=
+constant.log(A)
+
+//Declare four variables with assigned values
+let Name= adebowale
+let lastname= Tijani
+let country= Nigeria
+let age= 50
+
+//Declare variables to store your first name, last name, marital status, country and age in a single line//
+let Name= adebowale, lastname= Tijani, maritalstatus= single, country= Nigeria, age= 50
+console.log(firstName, lastname, maritalstatus, country, age)
+
+
+//Declare two variables myAge and yourAge and assign them initial values and log to the browser console
+let myAge= 35
+let yourAge= 52
+console.log(myAge, yourAge)
---
+++ variable.js
@@ -0,0 +1,16 @@
+//Write a single line comment which says, comments can make code readable//
+//comments can make code readable//
+//Write another single comment which says, Welcome to 30DaysOfJavaScript//
+// welcome to 30DaysOfJavaScript//
+
+//Write a multiline comment which says, comments can make code readable, easy to reuse and informative//
+/* comments can make code readable;
+    easy to reuseand informative
+*/
+
+//Create a variable.js file and declare variables and assign string, boolean, undefined and null data types//
+let x= Number
+var adebowale= String
+var boolean
+var emptyvalue= null
+var firstname
